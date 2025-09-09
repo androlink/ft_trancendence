d84c8f97042f511b341bd5c8c2350aaa1d41c044
ma-gabriel <gabi.eymat@gmail.com>
2025-09-09 20:28:20 +0200

    save before experimentation

--- script/setup.sh
+++ script/setup.sh
@@ -3,3 +3,4 @@
 mkdir -p secrets
 mkdir -p data
 mkdir -p data/certificate
+mkdir -p data/www
--- src/client/ts/app.ts
+++ src/client/ts/app.ts
@@ -3,62 +3,65 @@ import { templates } from "./templates.js"
 interface ServerResponse {
 	template?: string,
 	title?: string,
-	replace?: {[key:string]:string}
+	replace?: {[key:string]:string},
+	inner?: string
 }
 
 function keyExist(dict:object, key:PropertyKey) : boolean {
-	return Object.hasOwn(dict, key)
+	return Object.hasOwn(dict, key);
 }
 
 async function fetchApi(): Promise<ServerResponse> {
 	const response = await fetch(window.location.origin
 				     + "/api" + window.location.pathname);
 	const data: ServerResponse = await response.json();
-	return data
+	return data;
 }
 
 window.addEventListener('popstate', function(event) {
-	main()
+	main();
 }, false);
 
 function setHTML(element: HTMLElement, text: string) : void {
-	element.innerHTML = text
+	element.innerHTML = text;
 }
 
 function replaceElements(toReplace: {[key:string]:string}) : void {
 	for (const key in toReplace) {
-		setHTML(document.getElementById(key), toReplace[key])
+		setHTML(document.getElementById(key), toReplace[key]);
 	}
 }
 
+let mainTemplate: string | null = null;
+let mainInner: string | null = null;
 async function main() {
-	const data: ServerResponse = await fetchApi()
-	const app: HTMLElement | null = document.getElementById("app")
+	const data: ServerResponse = await fetchApi();
+	const app: HTMLElement | null = document.getElementById("app");
 	if (app === null)
-		return
+		return;
 	if (keyExist(data, "title")) {
 		document.title = data.title;
 	}
-	if (keyExist(data, "template") && data.template != main.template) {
-		main.template = data.template
+	if (keyExist(data, "template") && data.template !== mainTemplate) {
+		mainTemplate = data.template;
 		if (keyExist(templates, data.template)) {
-			setHTML(app, templates[data.template])
+			setHTML(app, templates[data.template]);
 			if (keyExist(data, "replace"))
-				replaceElements(data.replace)
+				replaceElements(data.replace);
 		} else {
 			app.innerHTML = "Template " +
-				data.template + " not Found in template.js"
+				data.template + " not Found in template.js";
 		}
 	}
 	const inner: HTMLElement | null = document.getElementById("inner");
-	if (inner && keyExist(data, "inner") && keyExist(templates, data.inner) && data.inner != main.inner){
-		main.inner = data.inner
-		setHTML(inner, templates[data.inner])
+	if (inner && keyExist(data, "inner") && keyExist(templates, data.inner) && data.inner != mainInner){
+		mainInner = data.inner;
+		setHTML(inner, templates[data.inner]);
 	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	main().catch(err => console.error(err))})
+	main().catch(err => console.error(err))});
 
 
 // for moving page to page, used by html
