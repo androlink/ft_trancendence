698636b3a4508457889d47674c8f0138ac747543
ma-gabriel <gabi.eymat@gmail.com>
2025-09-10 03:03:45 +0200

    Docker client restarting fixed

--- src/client/script/client.sh
+++ src/client/script/client.sh
@@ -1,6 +1,7 @@
+set -bm # without that f***ing line --wait doesn't work in background
 tsc --watch -p ts &
 pid1=$!
-tailwindcss -i css/input.css -o /var/www/styles.css --watch &
+tailwindcss -i css/input.css -o /var/www/styles.css --watch=always &
 pid2=$!
 wait -n $pid1 $pid2
 kill $pid1 2>/dev/null
