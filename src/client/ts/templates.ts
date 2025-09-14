5c2ea086c494abf9f68796726c123b59a74011ed
ma-gabriel <gabi.eymat@gmail.com>
2025-09-14 13:58:47 +0200

    better animation spacing and few improvements

--- src/client/ts/templates.ts
+++ src/client/ts/templates.ts
@@ -12,16 +12,20 @@ export const templates = {
 	`,
 	"Home":
 		`
-	<div class="flex justify-between space-x-2 mx-8 my-2">
-		<div class="flex">
-			<input type="text" placeholder="login" class="text-sm rounded-lg block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white checked:ring-blue-500 checked:border-blue-500"/>
-		</div>
-		<div class="flex space-x-2">
-			<img src="notification-icon.png" class="invert-50 size-10 cursor-pointer">
-			<img src="settings-icon.png" class="invert-50 size-10 cursor-pointer">
-		</div>
-	</div>
-	<div class="flex space-x-2 mx-8">
+	<span class="grid grid-cols-3 mx-8 my-2">
+		<span class="flex justify-self-start">
+			<input type="text" placeholder="login" class="text-sm select-none rounded-lg block p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:outline focus:ring-blue-500 focus:border-blue-500"/>
+		</span>
+		<p class="justify-self-center self-center text-3xl font-mono text-blue-900 font-semibold select-none">ft_transcendence</p>
+		<span class="justify-self-end flex space-x-2">
+			<span class="relative size-x-10">
+				<img src="notification-icon.png" class="select-none invert-50 hover:invert-75 size-10 cursor-pointer">
+				<span class="absolute top-0 right-0 inline-flex size-2 animate-ping rounded-full bg-sky-400 opacity-75"></span>
+			</span>
+			<img src="settings-icon.png" class="invert-50 select-none hover:animate-spin hover:invert-75 size-10 cursor-pointer">
+		</span>
+	</span>
+	<span class="flex space-x-2 mx-8">
 		<label class="cursor-pointer">
 			<input type="radio" name="option" class="peer hidden">
 			<div onclick="goToURL( )"		class="cursor-pointer peer-checked:cursor-default select-none rounded hover:border-transparent peer-checked:text-white peer-checked:bg-gray-500 bg-gray-700 text-gray-300">&nbsp;pdf&nbsp;</div>
@@ -32,14 +36,14 @@ export const templates = {
 		</label>
     	<label class="cursor-pointer">
 			<input type="radio" name="option" class="peer hidden">
-			<div onclick="goToURL('video')"class="cursor-pointer peer-checked:cursor-default select-none rounded hover:border-transparent peer-checked:text-white peer-checked:bg-gray-500 bg-gray-700 text-gray-300">&nbsp;video&nbsp;</div>
+			<div onclick="goToURL('video')"	class="cursor-pointer peer-checked:cursor-default select-none rounded hover:border-transparent peer-checked:text-white peer-checked:bg-gray-500 bg-gray-700 text-gray-300">&nbsp;video&nbsp;</div>
 		</label>
-	</div>
-	<div id="inner" class="flex-1 mx-8 mb-8 mt-4"></div>
+	</span>
+	<div id="inner" class="flex-1 mx-8 mb-8 mt-4 select-none"></div>
 	`,
 	"Game":
 		`
-	<iframe src="https://ivark.github.io/AntimatterDimensions/" class="size-full border-transparent" id="game" allowfullscreen="yes" allow="clipboard-read; clipboard-write; autoplay; execution-while-not-rendered; execution-while-out-of-viewport; gamepad" sandbox="allow-modals allow-pointer-lock allow-scripts allow-downloads allow-orientation-lock allow-popups allow-same-origin allow-forms" class="svelte-1qgtsbq game-filtered"></iframe>
+	<iframe src="https://ivark.github.io/AntimatterDimensions/" class="size-full border-transparent outline" id="game" allowfullscreen="yes" allow="clipboard-read; clipboard-write; autoplay; execution-while-not-rendered; execution-while-out-of-viewport; gamepad" sandbox="allow-modals allow-pointer-lock allow-scripts allow-downloads allow-orientation-lock allow-popups allow-same-origin allow-forms" class="svelte-1qgtsbq game-filtered"></iframe>
 		`,
 	"Video":
 		`
