9b0fce1d880ee85fd3019bb82f7a7a29ee4f80e5
gcros <guilhem.cros.38@gmail.com>
2025-09-24 16:43:18 +0200

    breakpoint work but dont know how to do anythings

--- src/client/game.ts
+++ src/client/game.ts
@@ -13,9 +13,16 @@ class Point {
 type Ray = [Point, Point];
 
 class CollideEvent {
+
+	constructor(point: Point, dist: {ray: number , segment: number}, segment: Segment| null)
+	{
+		this.point = point;
+		this.dist = dist;
+		this.segment = segment;
+	}
 	point: Point;
 	dist: {ray: number , segment: number};
-	segment: CollideSegment| null;
+	segment: Segment | null;
 };
 
 function mean_angle(angles: number[])
@@ -26,11 +33,25 @@ function mean_angle(angles: number[])
 	return Math.atan2(y, x);
 }
 
-class CollideSegment {
-	segment: [Point, Point];
+type CollideEffect = {f: Function, args: any[]}
 
-	public constructor(p1: Point, p2: Point) {
+function createEffect(f: Function, ...args: any[]): CollideEffect
+{
+	return {f, args};
+}
+
+class Segment {
+	segment: [Point, Point];
+	effect: CollideEffect | null;
+	public applyEffect(ball: PongBall, event: CollideEvent)
+	{
+		if (this.effect != null)
+			this.effect.f(ball, event, ...this.effect.args);
+	}
+
+	public constructor(p1: Point, p2: Point, effect: CollideEffect | null = null) {
 		this.segment = [p1, p2];
+		this.effect = effect
 	}
 
 	public bsp(p: Point): number{
@@ -44,11 +65,11 @@ class CollideSegment {
 		return Math.atan2(this.segment[1].y - this.segment[0].y, this.segment[1].x - this.segment[0].x) + Math.PI * .5;
 	}
 
-	public getMiddle(): Point
+	public getMiddle(adv: number = .5): Point
 	{
 		return new Point(
-			(this.segment[0].x - this.segment[1].x) * 0.5 + this.segment[1].x,
-			(this.segment[0].y - this.segment[1].y) * 0.5 + this.segment[1].y
+			(this.segment[0].x - this.segment[1].x) * adv + this.segment[1].x,
+			(this.segment[0].y - this.segment[1].y) * adv + this.segment[1].y
 		)
 	}
 
@@ -69,20 +90,16 @@ class CollideSegment {
 
 		if (s < (0 - 0.001) || s > (1 + 0.001)) return null;
 
-		return {
-				point: {
-					x: s * (B.x - A.x) + A.x, 
-					y: s * (B.y - A.y) + A.y,
-				},
-				dist: {
-					ray: r,
-					segment: s
-				},
-				segment: this
-		};
+		return new CollideEvent(
+			new Point(s * (B.x - A.x) + A.x, s * (B.y - A.y) + A.y), 
+			{
+				ray: r,
+				segment: s
+			},
+			this);
 	}
 
-	static intersectSegments(segments: CollideSegment[], ray: Ray)
+	static intersectSegments(segments: Segment[], ray: Ray)
 	{
 		return segments.map((s) => {
 			return s.intersect(ray)
@@ -90,23 +107,16 @@ class CollideSegment {
 			return s != null
 		});
 	}
-	static getClosestSegment(segments: CollideSegment[], ray: Ray)
+	static getClosestSegment(segments: Segment[], ray: Ray)
 	{
-		return CollideSegment.intersectSegments(segments, ray).sort((a, b) => {
+		return Segment.intersectSegments(segments, ray).sort((a, b) => {
 			return (a.dist.ray - b.dist.ray)
 		}).pop();
 	}
 }
 
-class CollideObject
+class PongBall
 {
-	segments: CollideSegment[] = [];
-
-	public getSegments() {return this.segments;}
-
-}
-
-class PongBall extends CollideObject{
 	location: Point;
 	angle: number;
 	speed: number;
@@ -115,7 +125,6 @@ class PongBall extends CollideObject{
 
 	constructor(location: Point|null, angle: number|null, speed: number|null, size: number|null, accuracy: number|null)
 	{
-		super();
 		this.location = location ??= new Point(0, 0);
 		this.angle = angle ??= 0;
 		this.speed = speed ??= 0;
@@ -140,7 +149,7 @@ class PongBall extends CollideObject{
 		return points
 	}
 
-	public update(segments: CollideSegment[])
+	public update(segments: Segment[])
 	{
 		console.log("---update---");
 		if (this.speed < 0)
@@ -162,13 +171,13 @@ class PongBall extends CollideObject{
 			{
 				let collidePoints = this.getCollidePoints();
 				collidePoints.forEach((point) => {
-					let usableSegment = segments.filter((s) => {return s.bsp(this.location) >= 0});
+					let usableSegment = segments.filter((s) => {return s.bsp(this.location) > 0});
 					let targetPoint = new Point(point.x + deltaPos.x, point.y + deltaPos.y);
-					let events = CollideSegment.intersectSegments(usableSegment, [this.location, targetPoint]);
+					let events = Segment.intersectSegments(usableSegment, [this.location, targetPoint]);
 					collision.push(...events);
 				})
 			}
-			collision.sort((a, b) => {return (a.dist.ray - b.dist.ray)})
+			collision = collision.filter((s)=>{return s.segment.effect != null}).sort((a, b) => {return (a.dist.ray - b.dist.ray)})
 			if (collision.length == 0)
 			{
 				this.location = {x: this.location.x + deltaPos.x,
@@ -179,60 +188,127 @@ class PongBall extends CollideObject{
 			{
 				console.log(JSON.stringify(collision));
 				let lastCollide = collision[0];
-
-				console.log(JSON.stringify(lastCollide));
 				this.location.x += Math.cos(this.angle) * remainder * lastCollide.dist.ray;
 				this.location.y += Math.sin(this.angle) * remainder * lastCollide.dist.ray;
 				remainder *= 1 / ( 1 + lastCollide.dist.ray);
-				
-				this.angle = 2 * (lastCollide.segment.getNormal() - Math.PI / 2) - this.angle;
+				lastCollide.segment.applyEffect(this, lastCollide);
+				// this.angle = 2 * (lastCollide.segment.getNormal() - Math.PI / 2) - this.angle;
 			}
 		}
+		if (count == 100)
+			console.error("PongBall: infinite loop detected");
 	}
 }
 
+let simpleBounce : Function = (ball: PongBall, ce: CollideEvent) =>
+{
+	ball.angle = 2 * (ce.segment.getNormal() - Math.PI / 2) - ball.angle;
+}
+
+let paddleBounce : Function = (ball: PongBall, ce: CollideEvent, player: PongPlayer) =>
+{
+	ball.angle = ce.segment.getNormal() - (ce.dist.segment - 0.5) * 2;
+}
+
+class PongPlayer
+{
+	constructor(path: Segment, size: number)
+	{
+		this.movePath = path;
+		this.size = size;
+	}
+
+	angle : number = 0;
+	movePath : Segment;
+	position: number = 0.5;
+	size: number;
+	speed: number;
+
+	public getPaddle() : Segment
+	{
+		let p1: Point, p2: Point;
+		const pos = this.movePath.getMiddle(this.position);
+		const pathAngle = this.angle + this.movePath.getNormal() - Math.PI / 2;
+
+		p1 = new Point(
+			-Math.cos(pathAngle) * this.size / 2 + pos.x,
+			-Math.sin(pathAngle) * this.size / 2 + pos.y);
+		p2 = new Point(
+			Math.cos(pathAngle) * this.size / 2 + pos.x,
+			Math.sin(pathAngle) * this.size / 2 + pos.y);
+		return new Segment(p1, p2, createEffect(paddleBounce, this));
+	};
+}
+
 class PongBoard {
-	segments: CollideSegment[];
+	segmentsList: Segment[] = [];
 	ball: PongBall;
+	player: PongPlayer[] = [];
+	count: number = 0;
+
+	public getSegments(): Segment[]
+	{
+		let testingSegment = [];
+
+		testingSegment.push(...this.segmentsList);
+		this.player.forEach((p) => {testingSegment.push(p.getPaddle());})
+		return testingSegment;
+	}
+
+	public update()
+	{
+		let testingSegment = this.getSegments();
+		this.count++;
+		this.player.forEach((p) => {
+			//p.position = (Math.cos(this.count /100) + 1) * .5;
+			p.position = 0.4;
+			if (p.position > 1)
+				p.position -= 1;
+		});
+		this.ball.speed = document.getElementById("ballSpeed").value;
+		this.ball.update(testingSegment);
+	}
+	public init()
+	{
+		const maxWall = 8;
+		const size = 100;
+		const angle = Math.PI * 2 / maxWall;
+		const defaultAngle = -Math.PI / 2;
+
+		this.segmentsList = [];
+		let points = [];
+		for (let i = 0; i < maxWall; i++)
+		{
+			let mult = i % 2;
+			points.push(new Point(
+				(100+Math.cos(i*angle + defaultAngle)*(size)),
+				(100+Math.sin(i*angle + defaultAngle)*(size))
+			));
+		}
+		for (let i = 0; i < maxWall; i++)
+		{
+			this.segmentsList.push(new Segment(points[i],points[(i + 1 + maxWall) % maxWall], createEffect(simpleBounce)));
+		}
+
+		this.ball = new PongBall({x: 100, y: 100}, 0, 1, 5, 6);
+
+		this.player = [];
+		this.player.push(new PongPlayer(new Segment(new Point(150, 50), new Point(150,150)), 50));
+		this.player.push(new PongPlayer(new Segment(new Point(50, 150), new Point(50,50)), 50));
+
+		this.count = 0;
+	}
 }
 
 let intervalId = 0;
 
-let segmentList: CollideSegment[] = [];
-
-let pongBall: PongBall = new PongBall(null, null, null, null, 3);
+let board: PongBoard = new PongBoard();
 
 function initGame()
 {
-	const maxWall = 80;
-	const size = 100;
-	const angle = Math.PI * 2 / maxWall;
-	const defaultAngle = -Math.PI / 2;
-
-	segmentList = [];
-	let points = [];
-	for (let i = 0; i < maxWall; i++)
-	{
-		let mult = i % 2;
-		points.push(new Point(
-			(100+Math.cos(i*angle + defaultAngle)*(size - 80 * mult)),
-			(100+Math.sin(i*angle + defaultAngle)*(size - 80 * mult))
-		));
-	}
-	for (let i = 0; i < maxWall; i++)
-	{
-		segmentList.push(new CollideSegment(points[i],points[(i + 1 + maxWall) % maxWall]));
-	}
-
-	for (let i = 0; i < 0; i++)
-	{
-		segmentList.push(new CollideSegment({x: Math.random() * 200, y: Math.random() * 200}, {x: Math.random() * 200, y: Math.random() * 200}));
-	}
-
-	pongBall = new PongBall({x: 100, y: 100}, 0, 1, 5, 6);
-
+	board.init();
 	clearInterval(intervalId)
-	intervalId = setInterval(game, 50);
+	intervalId = setInterval(game, 1000/30);
 
 }
 
@@ -240,13 +316,13 @@ initGame();
 
 function game()
 {
-	pongBall.speed = document.getElementById("ballSpeed").value;
-	pongBall.update(segmentList);
-	draw(segmentList, pongBall);
+	for (let i = 0; i < 1; i++)
+		board.update();
+	draw(board.getSegments(), board.ball);
 }
 
 
-function draw(segments: CollideSegment[], ball: PongBall)
+function draw(segments: Segment[], ball: PongBall)
 {
 	let canvas :HTMLCanvasElement = (document.getElementById("canvas") as HTMLCanvasElement);
 	let ctx = canvas.getContext("2d");
@@ -254,29 +330,19 @@ function draw(segments: CollideSegment[], ball: PongBall)
 	canvas.height = 200;
 	segments.forEach((segment) => {
 		if (segment.bsp(ball.location) > 0){
-			ctx.beginPath();
-			ctx.moveTo(segment.segment[0].x, segment.segment[0].y);
-			ctx.lineTo(segment.segment[1].x, segment.segment[1].y);
-			ctx.closePath();
-			ctx.stroke();
+			drawSegment(ctx, segment, "#000000");
 		}
 		let mid = segment.getMiddle();
-		ctx.beginPath();
-		ctx.save();
-		ctx.strokeStyle = '#ff0000'
-		ctx.moveTo(mid.x, mid.y);
-		ctx.lineTo(mid.x + Math.cos(segment.getNormal()) * 5, mid.y + Math.sin(segment.getNormal()) * 5);
-		ctx.closePath();
-		ctx.stroke();
-		ctx.restore();
+
+		drawSegment(ctx, new Segment(mid, new Point(mid.x + Math.cos(segment.getNormal()) * 5, mid.y + Math.sin(segment.getNormal()) * 5)), "#ff0000");
 	});
-	// CollideSegment.intersectSegments(segments,ray).forEach((se) => {
+	// Segment.intersectSegments(segments,ray).forEach((se) => {
 	// 	 ctx.beginPath();
 	// 	ctx.arc(se.point.x, se.point.y, 3, 0, 2 * Math.PI);
 	// 	ctx.closePath();
 	// 	ctx.fill();
 	// });
-	// let s = CollideSegment.getClosestSegment(segments, ray);
+	// let s = Segment.getClosestSegment(segments, ray);
 	// if (s != null)
 	// {
 	// 	ctx.beginPath();
@@ -295,12 +361,17 @@ function draw(segments: CollideSegment[], ball: PongBall)
 		let deltaPos: Point = {x: Math.cos(ball.angle) * ball.speed,
 			y: Math.sin(ball.angle) * ball.speed};
 		ball.getCollidePoints().forEach((p) => {
-			ctx.beginPath();
-			//ctx.arc(p.x + deltaPos.x, p.y + deltaPos.y, 2, 0, 2 * Math.PI);
-			ctx.moveTo(p.x, p.y);
-			ctx.lineTo(p.x + deltaPos.x, p.y + deltaPos.y);
-			ctx.closePath();
-			ctx.stroke();
+			drawSegment(ctx, new Segment(p, new Point(p.x + deltaPos.x, p.y + deltaPos.y)), "#0000ff");
 		})
 	}
+}
+
+function drawSegment(ctx: CanvasRenderingContext2D,segment: Segment, color: string | CanvasGradient | CanvasPattern)
+{
+	ctx.beginPath();
+	ctx.moveTo(segment.segment[0].x, segment.segment[0].y);
+	ctx.lineTo(segment.segment[1].x, segment.segment[1].y);
+	ctx.closePath();
+	ctx.strokeStyle = color;
+	ctx.stroke();
 }
