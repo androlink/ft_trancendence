a0f0036fb6c8a6970352857cd385316a0b458a20
ma-gabriel <gabi.eymat@gmail.com>
2025-10-04 20:21:50 +0200

    fixed client docker taking too much time to stop (using trap command)

--- src/client/script/client.sh
+++ src/client/script/client.sh
@@ -3,6 +3,7 @@ tsc --watch -p ts &
 pid1=$!
 tailwindcss -i css/input.css -o /var/www/styles.css --watch=always &
 pid2=$!
+trap "kill $pid1; kill $pid2" SIGTERM   SIGKILL
 wait -n $pid1 $pid2
 kill $pid1 2>/dev/null
 kill $pid2 2>/dev/null
