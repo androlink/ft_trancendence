b8e9b5f22d18777662389c4af76e707bdb2e10be
gcros <guilhem.cros.38@gmail.com>
2025-12-05 00:46:50 +0100

    add JWT to remote and chat route

--- services/server/src/micro_chat/main.ts
+++ services/server/src/micro_chat/main.ts
@@ -2,8 +2,16 @@ import fastifyModule from "fastify";
 import fastifyWebSocket from "@fastify/websocket";
 import fastifyJWT from "@fastify/jwt";
 
+import { Id, JwtUserPayload } from "../common/types";
 import chatRoute from "./chat_route";
 import { initDB } from "../common/database";
+
+declare module "@fastify/jwt" {
+  interface FastifyJWT {
+    payload: Id;
+    user: JwtUserPayload;
+  }
+}
 
 // if changed for better naming convention
 // need to be changed in page.html and template too
--- services/server/src/micro_remote/main.ts
+++ services/server/src/micro_remote/main.ts
@@ -2,8 +2,16 @@ import fastifyModule from "fastify";
 import fastifyWebSocket from "@fastify/websocket";
 import fastifyJWT from "@fastify/jwt";
 
+import { Id, JwtUserPayload } from "../common/types";
 import { initDB } from "../common/database";
 import apiRemote from "./remote_route";
+
+declare module "@fastify/jwt" {
+  interface FastifyJWT {
+    payload: Id;
+    user: JwtUserPayload;
+  }
+}
 
 // if changed for better naming convention
 // need to be changed in page.html and template too
