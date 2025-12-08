e3daee442fbd0a53df3e1e6c988e59d5231deb97
gcros <guilhem.cros.38@gmail.com>
2025-12-08 21:28:49 +0100

    dont work
    it work before

--- services/server/src/micro_chat/chat_invite.ts
+++ services/server/src/micro_chat/chat_invite.ts
@@ -1,5 +1,10 @@
 import { WebSocket } from "ws";
-import { WSmessage, TypeMessage } from "../common/types";
+import { WSmessage, TypeMessage, UserRow, Id } from "../common/types";
+
+import db from "../common/database";
+import { fastify } from "./main";
+import { send } from "process";
+import { req } from "pino-std-serializers";
 
 const request_url = "http://remote_microservice:3000/api/create";
 
@@ -8,19 +13,35 @@ export async function invite_message(event: WebSocket.MessageEvent) {
 
   if (msg.type !== TypeMessage.invite) return;
 
-  const request = {
-    method: "POST",
-    body: JSON.stringify({
-      player1: 0,
-      player2: 0,
-    }),
-  };
+  console.log(msg);
   try {
+    const get_id_from_name = db?.prepare<{ username: string }, UserRow>(
+      "SELECT * FROM users WHERE username = :username"
+    );
+    const sender: { id: Id } | null = fastify.jwt.decode(msg.user);
+    if (sender === null) throw new Error("invalid token");
+    const player1 = sender.id;
+    const player2 = get_id_from_name.get({ username: msg.target! });
+    if (player2 === undefined)
+      throw new Error(`cannot found player: ${msg.target}`);
+
+    const request = {
+      method: "POST",
+      body: JSON.stringify({
+        player1: player1,
+        player2: player2.id,
+      }),
+    };
+    console.log(request.body);
     const response = await fetch(request_url, request);
-    if (!response.ok) return;
-    const result = response.json();
+    if (!response.ok) {
+      throw new Error(`error on request: ${response.status}`);
+    }
+    const result = await response.json();
+    console.log(result.game_id);
     //todo do something with result;
   } catch (e) {
+    event.target.send(JSON.stringify({ msg: "cant generate party" }));
     console.error("invite:", e);
   }
 }
--- services/server/src/micro_remote/create_route.ts
+++ services/server/src/micro_remote/create_route.ts
@@ -3,6 +3,7 @@ import fastifyFormbody from "@fastify/formbody";
 import fastifyMultipart from "@fastify/multipart";
 import { Id } from "../common/types";
 import { pong_party_create } from "./pong_party";
+import db from "../common/database";
 
 export default async function createRoute(fastify: FastifyInstance) {
   fastify.register(fastifyMultipart);
@@ -11,8 +12,9 @@ export default async function createRoute(fastify: FastifyInstance) {
     "/create",
     async (req, reply) => {
       if (req.body === undefined) return reply.code(400);
+      console.log(req.body);
       let party = pong_party_create([req.body.player1, req.body.player2]);
-      if (party === undefined) return reply.code(500);
+      if (!party) return reply.code(500);
       console.log(`new party id: ${party.getId()}`);
       return reply.code(200).send(
         JSON.stringify({
